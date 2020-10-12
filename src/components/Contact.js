dab51a6d16be3f39a2b5e4fb1bda8c8289ce62e1
anpineau <an.pineau12@gmail.com>
2020-10-12 17:33:56 +0200

    tests 2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,8 +6,13 @@ export default React.forwardRef((props, ref) => {
         <div ref={ref} className="Contact"> 
             <Title title="Contact" />
 
-            <form name="contact" method="post" className="form" data-sal="zoom-in" data-sal-duration="1000">
-                <input type="hidden" name="contact" value="contact" />
+            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" className="form" data-sal="zoom-in" data-sal-duration="1000">
+                <input type="hidden" name="form-name" value="contact" />
+                <div hidden>
+                    <label>
+                        <input name="bot-field" />
+                    </label>
+                </div>
                 <input type="text" className="name" placeholder="Name"/>
                 <input type="text" className="email" placeholder="Email"/>
                 <textarea name="message" className="message" placeholder="Your message"></textarea>
